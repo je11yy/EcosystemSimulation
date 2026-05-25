88bcf5f8a78a780669b3e1d08985fd3f13ac524e
je11yy <l4da-vlasov@yandex.ru>
2026-05-25 18:03:53 +0300

    Улучшает верхнее меню

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Menu } from "src/components/Menu";
 import { AppProviders } from "./app/providers";
 
 export function App() {
+    const location = useLocation();
+    console.log(location.pathname);
+    const itemName = location.pathname.slice(1) + '-menu-item';
+    const menuItems = document.querySelectorAll('.simulations-menu-item, .genomes-menu-item');
+    menuItems.forEach(item => {
+        if (item.classList.contains(itemName)) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+
     return (
         <AppProviders>
             <div className="app-shell">
